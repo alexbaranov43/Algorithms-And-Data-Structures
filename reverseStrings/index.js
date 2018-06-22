506e42bab6e8ce32fd037e537c09989979f01fa4
alexbaranov43 <alexbaranov43@gmail.com>
2018-06-22 19:15:43 -0400

    reverse string

---
+++ reverseStrings/index.js
@@ -0,0 +1,13 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+reverse = str => {console.log(str.split('').reverse().join(''))}
+
+reverse('apple')
+reverse('hello')
+reverse('Greetings!')
